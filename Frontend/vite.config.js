e42471545876dcc52e4567e27f028a42666df8be
farihaa1 <farihaa1403@gmail.com>
2025-04-15 22:23:55 +0600

    added shadcn

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,10 +4,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { fileURLToPath } from 'url'
 
-// fix for __dirname in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
