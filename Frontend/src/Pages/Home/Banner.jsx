8105417f5d28a545b78df29e1a6d819a935f53c3
farihaa1 <farihaa1403@gmail.com>
2025-04-23 21:22:50 +0600

    .

--- Frontend/src/Pages/Home/Banner.jsx
+++ Frontend/src/Pages/Home/Banner.jsx
@@ -10,7 +10,7 @@ import BannerCard from "./BannerCard";
 const Banner = () => {
   return (
     <div className="flex flex-col md:flex-row  justify-start items-center md:h-[35rem] gap-4">
-      <div className="w-full md:w-3/5 flex flex-col md:pl-12 ">
+      <div className="w-full md:w-3/5 flex flex-col  md:pl-12 ">
         <h2 className="capitalize text-3xl  text-center md:text-start md:text-5xl mt-4 lg:mt-0 lg:text-6xl font-black pb-4 md:leading-13 lg:leading-16 ">
           Delivery <br /> in <span className="text-red-700">30 minutes</span>
           <br /> at your doorstep
@@ -23,10 +23,10 @@ const Banner = () => {
 
         <div></div>
       </div>
-      <div className="w-full z-0 md:w-2xl md:z-20 relative ">
-        <div className=" bg-red-800 px-2 md:pt-0 z-0 md:px-0 md:z-20 rounded-tl-full  ">
-          <Lottie animationData={bannerLottie} loop={true} autoplay={true} />
-          <div className="absolute top-96 md:top-120 z-10 right-64 md:right-90 animate animate-float">
+      <div className="w-full bg-red-800 z-0 md:w-2xl md:rounded-tl-full  md:z-20 relative">
+        <div className="md:pt-0 z-0 md:px-0 md:z-20  ">
+          <Lottie className="w-sm flex md:w-full" animationData={bannerLottie} loop={true} autoplay={true} />
+          <div className="absolute top-54 right-50 md:top-96 lg:top-120 z-10  md:right-90 animate animate-float">
             <Comment
               icon={
                 <FaCheck className="text-white p-1 bg-green-500 rounded-full" />
@@ -34,7 +34,7 @@ const Banner = () => {
               heading={"Delivery Complete"}
             />
           </div>
-          <div className="absolute top-50 right-10 animate-float">
+          <div className="absolute top-40 left-0  md:top-50 md:right-10 animate-float">
             <Comment
               icon={
                 <RiTimerFlashLine className="text-white p-1  bg-green-500 rounded-full text-xl md:text-4xl" />
@@ -42,11 +42,11 @@ const Banner = () => {
               heading={"Delivery 30 Min"}
             />
           </div>
-          <div className="absolute flex md:top-140">
+          <div className="absolute flex top-10 right-10 md:top-140">
             <div className=" w-32 md:w-56  z-10 rotate-[20deg] top-90 ">
               <Lottie animationData={arrowLottie} loop={true} autoplay={true} />
             </div>
-            <div className="w-l md:w-xs md:top-90">
+            <div className="w-l md:w-xs ">
               <BannerCard></BannerCard>
             </div>
           </div>
